2de88030c2e7c800e3c3b5af1897f58c4d019a47
christopherfelt <chris.h.felt@gmail.com>
2021-08-09 06:59:55 -0600

    added post components

---
+++ src/components/DisplayPosts.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+
+import DeletePost from "./DeletePost";
+import { listPosts } from "../graphql/queries";
+import EditPost from "./EditPost";
+
+class DisplayPosts extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount = async () => {
+    this.getPosts();
+  };
+
+  getPosts = async () => {
+    const result = await API.graphql(graphqlOperation(listPosts));
+
+    this.setState({ posts: result.data.listPosts.items });
+
+    // console.log("All Posts: ", result.data.listPosts.items);
+  };
+
+  render() {
+    const { posts } = this.state;
+    return posts.map((post) => {
+      return (
+        <div key={post.id} className="posts" style={rowStyle}>
+          <h1>{post.postTitle}</h1>
+          <span>
+            {"Wrote by: "}
+            {post.postOwnerUsername}
+            <p>{post.postBody}</p>
+            <br />
+            <span>
+              <DeletePost />
+              <EditPost />
+            </span>
+          </span>
+          <time style={{ fontStyle: "italic", color: "#0ca5e297" }}>
+            {" "}
+            {new Date(post.createdAt).toDateString()}
+          </time>
+        </div>
+      );
+    });
+  }
+}
+
+const rowStyle = {
+  background: "#f4f4f4",
+  padding: "10px",
+  margin: "14px",
+};
+
+export default DisplayPosts;
